96eb57c2020eab76b20a215c991e41b300326f42
EC2 Default User <ec2-user@ip-172-31-46-162.ec2.internal>
2019-01-20 06:25:05 +0000

    my second commit

---
+++ ascending.py
@@ -0,0 +1,10 @@
+a="678345"
+x,y=0,9
+str=''
+while y>=x:
+    for i in a:
+        if y==int(i):
+            str=str+i
+    y=y-1
+print(str)
+
---
+++ squaredigit.py
@@ -0,0 +1,10 @@
+x="9119"
+c=[]
+for i in x:
+    a=int(i)
+    b=a**2
+    b=str(b)
+    c.append(b)
+print(c)
+str=''.join(c)
+print(str)
